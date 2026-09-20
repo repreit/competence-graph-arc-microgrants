5856885a102b56fdfb8408e47fe316119a7abdf3
repreit <315808466+repreit@users.noreply.github.com>
2026-09-20 21:54:04 +0900

    Update app.js to replace config route with public-config route

--- impl/backend/nodejs/src/app.js
+++ impl/backend/nodejs/src/app.js
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import auth from "./routes/auth.js";
 import bindings from "./routes/bindings.js";
-import config from "./routes/config.js";
+import publicConfig from "./routes/public-config.js";
 
 const app = new Hono();
 
@@ -15,7 +15,7 @@ app.use(
     }),
 );
 
-app.route("/config", config);
+app.route("/public-config", publicConfig);
 app.route("/auth", auth);
 app.route("/bindings", bindings);
 
