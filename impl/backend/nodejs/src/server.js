77aff56f3bef7543fa1ec46c60b83ae73558d424
repreit <315808466+repreit@users.noreply.github.com>
2026-09-20 22:13:07 +0900

    Refactor server.js to import port configuration from config.js

--- impl/backend/nodejs/src/server.js
+++ impl/backend/nodejs/src/server.js
@@ -1,7 +1,6 @@
 import { serve } from "@hono/node-server";
 import app from "./app.js";
-
-const port = Number(process.env.PORT ?? "3000");
+import { port } from "./config.js";
 
 serve({ fetch: app.fetch, port }, (info) => {
     console.log(`http://127.0.0.1:${info.port}`);
