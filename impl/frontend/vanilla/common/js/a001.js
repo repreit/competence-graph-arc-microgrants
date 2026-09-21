7f43cd65d16b4ee327b7a482777df715bdc81c05
repreit <315808466+repreit@users.noreply.github.com>
2026-09-21 21:54:27 +0900

    Add utility functions for address formatting and API base retrieval

---
+++ impl/frontend/vanilla/common/js/a001.js
@@ -0,0 +1,29 @@
+import { api } from "../../config.js";
+
+export function shortAddress(address) {
+    if (!address) {
+        return "";
+    }
+    if (address.length < 12) {
+        return address;
+    }
+    return address.slice(0, 6) + "…" + address.slice(-4);
+}
+
+export function apiBase() {
+    const query = new URLSearchParams(location.search).get("api");
+    if (query) {
+        return query.replace(/\/$/, "");
+    }
+    if (
+        location.hostname === "localhost" ||
+        location.hostname === "127.0.0.1"
+    ) {
+        return api.local;
+    }
+    return api.base;
+}
+
+export function pageUri() {
+    return location.origin + location.pathname.replace(/\/+$/, "");
+}
