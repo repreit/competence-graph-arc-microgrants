af399d7a5c4e447860da113bbacb12e73ea2c9a8
repreit <315808466+repreit@users.noreply.github.com>
2026-09-21 16:10:23 +0900

    Update TODO in server.js to include SIGINT for graceful shutdown

--- impl/backend/nodejs/src/server.js
+++ impl/backend/nodejs/src/server.js
@@ -2,7 +2,7 @@ import { serve } from "@hono/node-server";
 import app from "./app.js";
 import { port } from "./config.js";
 
-// TODO: handle SIGTERM for graceful shutdown before self-hosting.
+// TODO: handle SIGTERM/SIGINT for graceful shutdown before self-hosting.
 serve({ fetch: app.fetch, port }, (info) => {
     console.log(`http://127.0.0.1:${info.port}`);
 });
