a83806254d61a5ec149c2b6626f9f6562e941616
repreit <315808466+repreit@users.noreply.github.com>
2026-09-21 10:05:54 +0900

    Add apiBase function to handle API URL retrieval based on location

---
+++ impl/frontend/vanilla/common/js/api-base.js
@@ -0,0 +1,13 @@
+export function apiBase() {
+    const query = new URLSearchParams(location.search).get("api");
+    if (query) {
+        return query.replace(/\/$/, "");
+    }
+    if (
+        location.hostname === "localhost" ||
+        location.hostname === "127.0.0.1"
+    ) {
+        return "http://127.0.0.1:3000";
+    }
+    return "";
+}
