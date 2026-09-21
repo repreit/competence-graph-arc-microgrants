9f21d91bc2d3b8710abefa7418f639e15a8d6237
repreit <315808466+repreit@users.noreply.github.com>
2026-09-21 21:51:26 +0900

    Remove unused address and API utility functions for code cleanup

--- impl/frontend/vanilla/common/js/address.js
+++
@@ -1,9 +0,0 @@
-export function shortAddress(address) {
-    if (!address) {
-        return "";
-    }
-    if (address.length < 12) {
-        return address;
-    }
-    return address.slice(0, 6) + "…" + address.slice(-4);
-}
